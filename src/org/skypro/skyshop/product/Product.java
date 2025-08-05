b6a568d5c596183cfab0162a4b6de19682196fda
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2025-08-05 23:38:10 +0300

    HomeWork version1.2 Create of new Interface and Classes

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,6 +1,7 @@
 package org.skypro.skyshop.product;
+import org.skypro.skyshop.search.Searchable;
 
-public class Product {
+public class Product implements Searchable {
     private final String nameOfProduct;
     private final int priceOfProduct;
     //создаём конструктр с параметрами
@@ -16,6 +17,23 @@ public class Product {
 
     public int getPriceOfProduct() {
         return priceOfProduct;
+    }
+
+    // //ПЕРЕОПРЕДЕЛЯЕМ МЕТЫ ИНТЕРФЕЙСА Т.К. ОБЯЗАНЫ+по заданию так
+    // Реализация методов интерфейса (строго по заданию)
+    @Override
+    public String getSearchTerm() {
+        return nameOfProduct; // Как указано: "возвращать имя товара"
+    }
+
+    @Override
+    public String getContentType() {
+        return "PRODUCT"; // Как требуется
+    }
+
+    @Override
+    public String getName() {
+        return nameOfProduct; // Имя товара
     }
 
 }
